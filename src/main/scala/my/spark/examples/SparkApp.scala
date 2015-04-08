1058c4cf560ba865a6b925a9468bb9378c3cc813
Matt Hagen <anonz3000@gmail.com>
2015-04-08 07:07:15 -0400

    Added command line check.

--- src/main/scala/my/spark/examples/SparkApp.scala
+++ src/main/scala/my/spark/examples/SparkApp.scala
@@ -4,6 +4,10 @@ import org.apache.spark.SparkContext
 
 object SparkApp {
   def main(args: Array[String]) {
+    if (args.length != 0) {
+      println("Usage: SparkApp")
+      System.exit(1)
+    }
     val sc = new SparkContext()
     println( "sc.appName = " + sc.appName)
     sc.stop()
