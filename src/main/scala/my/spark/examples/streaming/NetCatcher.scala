63d27e126944d8d7e1de71602d1c45684656fb7f
Matt Hagen <anonz3000@gmail.com>
2015-04-08 12:49:44 -0400

    Adding NetCatcher.

---
+++ src/main/scala/my/spark/examples/streaming/NetCatcher.scala
@@ -0,0 +1,31 @@
+package my.spark.examples.streaming
+
+import org.apache.spark.SparkConf
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.StreamingContext._
+
+object NetCatcher {
+  def main(args: Array[String]) {
+    if (args.length < 2 || args.length > 3) {
+      println("Usage: NetCatcher <host> <port> [directory]")
+      System.exit(1)
+    }
+    val host = args(0)
+    val port = args(1).toInt
+    val batchDuration = 2
+
+    val sparkConf = new SparkConf().setAppName("NetCatcher")
+    val ssc = new StreamingContext(sparkConf, Seconds(batchDuration))
+
+    val lines = ssc.socketTextStream(host, port)
+    val words = lines.flatMap(_.split(" "))
+    val counts = words.map(word => (word, 1)).reduceByKey(_ + _)
+    if (args.length == 3) {
+      counts.saveAsTextFiles(args(2))
+    } 
+    counts.print()
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
